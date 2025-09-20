4c67048f5169f35c3f077a00771ee02d17f1e306
carito-cc <carolina.caroca.c@gmail.com>
2025-09-20 10:00:39 +0200

    pairs et calculatrice

--- pairs_calculatrice.cs
+++ pairs_calculatrice.cs
@@ -1,6 +1,6 @@
 /*Demander à l'utilisateur d'encoder 1 nombre(int), si le modulo de 2 est 0, 
 afficher "le nombre est pair", sinon "le nombre est impair" et gerer les erreurs.*/
-using System;
+/*using System;
 
 Console.WriteLine("Entrez un numero entier: ");
 int numero = Convert.ToInt32(Console.ReadLine());
@@ -11,31 +11,12 @@ if (numero % 2 == 0)
 else
 {
     Console.WriteLine("Le nombre est impair.");
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+};
 
 /* Developez une calculatrice simple qui demande a l'utilisateur de saisir deux nombres entiers et une operation (+, -, *, /),
 puis effectue l'operation et affiche le resultat.
 */
-class Calculatrice
+/*class Calculatrice
 {
     static void Main()
     {
@@ -84,4 +65,15 @@ class Calculatrice
             Console.WriteLine($"Le resultat de {num1} {operation} {num2} est : {resultat}");
         }
     }
-}
+};
+
+*/
+/*Vérifier que'un compte bancaire BBAN est valide et afficher le résultat dans la console. 
+(Il est valide si le modulo des 10 premiers chiffres par 97 donne les 2 derniers ou ils
+valent 97 s'il vaut 0.) exemple valide: 539007547034 */
+
+Console.WriteLine("Entrez un numero de compte bancaire (BBAN) :");
+string bban = Console.ReadLine();
+
+
+
